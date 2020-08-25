0bd9ff12dfca0c9e78fdb6ed57079fc25cc1913a
Andre ALbuquerque <alalbuquerque22@gmail.com>
2020-08-25 16:06:51 -0300

    callbacks

--- funcao/arrowFunction3.js
+++ funcao/arrowFunction3.js
@@ -4,19 +4,17 @@ let comparaComThis = function(param) {
 
 
 // quando atribuito em uma variavel o this tem contexto global ... ENTAO TOMAR CUIDADO Ara NAO ALTERAR 
-comparaComThis(global)
+comparaComThis(global)// retorna true 
 
-const obj = {
-
-}
+const obj = { }
 comparaComThis = comparaComThis.bind(obj)
-comparaComThis(global)
-comparaComThis(obj)
+comparaComThis(global)//retorna false
+comparaComThis(obj)// retorna true devido ao bind
 
 let comparaComThisArrow = param => console.log(this == param)
-comparaComThisArrow(global)
-comparaComThisArrow(module.exports)
+comparaComThisArrow(global)//retorna false
+comparaComThisArrow(module.exports)//retorna true
 
 comparaComThisArrow = comparaComThisArrow.bind(obj)
-comparaComThisArrow(obj)
-comparaComThisArrow(module.exports)
+comparaComThisArrow(obj)// retorna falso, a arrow function eh mais forte 
+comparaComThisArrow(module.exports)// retorna true .... ele continua aoontando o obj descrito
